867027a4e8f16c8a7036801b1450ab67be113baa
David Tudor <67743358+dav1d2006@users.noreply.github.com>
2020-09-30 15:19:50 +0300

    Add files via upload

---
+++ david_health&armour/__resource.lua
@@ -0,0 +1,15 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+dependency "vrp"
+
+dependency "vrp_mysql"
+
+client_scripts{ 
+	"lib/Tunnel.lua",
+  "lib/Proxy.lua",
+  "client.lua"
+}
+
+server_scripts{ 
+  "@vrp/lib/utils.lua"
+}
